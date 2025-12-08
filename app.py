e0db218e9a85ad213c71306f8b68a874bd0f9b15
hond25 <168510097+hond25@users.noreply.github.com>
2025-12-08 15:27:23 +0900

    書き方修正

--- app.py
+++ app.py
@@ -89,7 +89,8 @@ def is_it_tech(title, url):
         '技術', 'IT', 'プログラミング', 'エンジニア', '開発', 'API', 'AI', '人工知能', 'Python', 'JavaScript', 
         'プログラム', 'システム', 'ソフトウェア', 'ハードウェア', 'クラウド', 'サーバ', 'データ', 'ネットワーク', 'セキュリティ', 'Web技術', 
         'IT技術', 'Google', 'Chrome', 'Takeout', 'GitHub', 'コード','API', 'AI', 'ML', '機械学習', 
-        'Deep Learning', 'チュートリアル', '基礎', 'HTML', 'CSS', 'React', 'フロントエンド', 'バックエンド']
+        'Deep Learning', 'チュートリアル', '基礎', 'HTML', 'CSS', 'React', 'フロントエンド', 'バックエンド'
+    ]
     title = title or ""
     url = url or ""
     for keyword in keywords:
